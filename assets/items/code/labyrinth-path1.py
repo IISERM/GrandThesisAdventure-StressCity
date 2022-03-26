bb386537ffd6a0f2a2f2240289f1de9d7014b25d
James471 <56861963+James471@users.noreply.github.com>
2022-03-27 04:05:10 +0530

    Add Labyrinth Files

---
+++ assets/items/code/labyrinth-path1.py
@@ -0,0 +1,6 @@
+game.surface("┈").walkable = ~game.surface("┈").walkable
+game.surface("┊").walkable = ~game.surface("┊").walkable
+
+message = f"Secret entrance is unlocked!" if(game.surface('┊').walkable) else f"Secret entrance is locked."
+
+sg.popup_no_buttons(message, auto_close = True, auto_close_duration = 2, no_titlebar = True, modal = True,  background_color = "#4D4D4D", keep_on_top = True)
