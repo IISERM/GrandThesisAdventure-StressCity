d616d85e85cf299296b7b372532a78e439298726
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-03-27 22:49:56 +0530

    Fixes buying mop without paying Fixes #8

--- assets/items/code/lata_mukherjee-pay.py
+++ assets/items/code/lata_mukherjee-pay.py
@@ -1,6 +1,6 @@
 game.map("Lata Mukherjee's Shop").remove_item("Pay", game)
-game.active_map.exits["0,36"] = ["SC",[48,63]]
-game.active_map.exit_coords.append([0,36])
+
+game.pocket.append(game.item("Mop"))
 
 game.map("Biswas Office").remove_item("CV", game)
 game.map("Biswas Office").place_item("Find PhD", game)
