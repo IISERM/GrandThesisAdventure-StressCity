cca97a4b4a2f33cd1c74133e177a39237fa349f5
Akshay <sakshays.2000@gmail.com>
2022-03-29 00:39:53 +0530

    added wrench unlock for labyrinth

---
+++ assets/items/code/caf-jammed-door.py
@@ -0,0 +1,14 @@
+message = ""
+
+if not ("Wrench" in game.pocket.itemlist):
+    message = "I need to find a wrench somewhere around here..."
+    game.active_map.place_item("Wrench", game)
+
+elif (game.active_map.fmt[29,131] == "⎢"):
+    message = "...-CREAAAAK-...\n That did the trick!"
+    game.active_map.fmt[[28, 29, 30], [131, 131, 131]] = "→"
+    game.active_map.fmt_ref[[28, 29, 30], [131, 131, 131]] = "→"
+
+    game.active_map.remove_item("Jammed door", game)
+
+sg.popup_no_buttons(message, auto_close = True, auto_close_duration = 2, no_titlebar = True, modal = True,  background_color = "#4D4D4D", keep_on_top = True)
