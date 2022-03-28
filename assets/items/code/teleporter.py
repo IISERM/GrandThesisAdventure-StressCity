ed2b89bbd8b7535fc3178938bfece833f161048e
James471 <56861963+James471@users.noreply.github.com>
2022-03-29 00:27:39 +0530

    Fix teleporter

--- assets/items/code/teleporter.py
+++ assets/items/code/teleporter.py
@@ -1,2 +1,2 @@
-game.active_map.pos = [10, 10]
+game.active_map = map("Hostel Room")
 sg.popup_no_buttons(f"Beam me up, Scotty!", auto_close = True, auto_close_duration = 2, no_titlebar = True, modal = True,  background_color = "#4D4D4D", keep_on_top = True)
