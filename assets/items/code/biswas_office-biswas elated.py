1f1fef7afe4af21fd290d646b26fbcaa455c72db
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2022-03-30 03:13:01 +0530

    Lata -> Atal fixes #56

--- assets/items/code/biswas_office-biswas elated.py
+++ assets/items/code/biswas_office-biswas elated.py
@@ -1,6 +1,6 @@
 sg.popup_no_buttons("Ughh... What a fantastic start of my masters thesis. Troubles keep finding me.", auto_close = True, auto_close_duration = 2, no_titlebar = True, modal = True,  background_color = "#4D4D4D", keep_on_top = True)
 
-if("Lata Mukherjee (Idle)" in game.map("Lata Mukherjee's Shop").items.values()):
-    game.map("Lata Mukherjee's Shop").remove_item("Lata Mukherjee (Idle)", game)
-    game.map("Lata Mukherjee's Shop").place_item("Lata Mukherjee (Expectant)", game)
+if("Atal Mukherjee (Idle)" in game.map("Atal Mukherjee's Shop").items.values()):
+    game.map("Atal Mukherjee's Shop").remove_item("Atal Mukherjee (Idle)", game)
+    game.map("Atal Mukherjee's Shop").place_item("Atal Mukherjee (Expectant)", game)
 
--- assets/items/code/lata_mukherjee-pay.py
+++ assets/items/code/lata_mukherjee-pay.py
@@ -1,4 +1,4 @@
-game.map("Lata Mukherjee's Shop").remove_item("Pay", game)
+game.map("Atal Mukherjee's Shop").remove_item("Pay", game)
 
 game.pocket.append(game.item("Mop"))
 
@@ -7,5 +7,5 @@ game.map("Biswas Office").place_item("Find PhD", game)
 
 sg.popup_no_buttons("You have bought a mop.", auto_close = True, auto_close_duration = 2, no_titlebar = True, modal = True,  background_color = "#4D4D4D", keep_on_top = True)
 
-game.map("Lata Mukherjee's Shop").remove_item("Lata Mukherjee (Expectant)", game)
-game.map("Lata Mukherjee's Shop").place_item("Lata Mukherjee (Tired)", game)
+game.map("Atal Mukherjee's Shop").remove_item("Atal Mukherjee (Expectant)", game)
+game.map("Atal Mukherjee's Shop").place_item("Atal Mukherjee (Tired)", game)
