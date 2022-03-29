96f181b04702dfb79ef1a161fab80a0cd62ba297
Akshay <sakshays.2000@gmail.com>
2022-03-30 02:26:54 +0530

    changed biswas states

--- main.py
+++ main.py
@@ -13,6 +13,6 @@ class MyGame(terminal.Game):
         self.window.bind("<KeyPress-Down>", "down")
         self.window.bind("<KeyPress-Right>", "right")
 
-myGame = MyGame("Turing Hunt 2022", "assets/settings.json", [gadgets.Clock, gadgets.GPS, CustomGadgets.StressMeter, CustomGadgets.Phone], "Hostel 7 Floor 1")
+myGame = MyGame("Turing Hunt 2022", "assets/settings.json", [gadgets.Clock, gadgets.GPS, CustomGadgets.StressMeter, CustomGadgets.Phone], "Hostel Room")
 
 myGame.run()
