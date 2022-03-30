380ac87284c4b9a9d765acb435d535cfe6501a63
James471 <56861963+James471@users.noreply.github.com>
2022-03-30 20:09:55 +0530

    Fix bug

--- assets/items/code/phd-active.py
+++ assets/items/code/phd-active.py
@@ -1,5 +1,5 @@
-game.gadgets.Phone.phdNotifications = True
-game.gadgets.Phone.mediaPath = "image::assets/media/phone/2.png"
+game.gadgets[3].Phone.phdNotifications = True
+game.gadgets[3].Phone.mediaPath = "image::assets/media/phone/2.png"
 game.active_map.remove_item("Gurpreet Sita Rahim Mary Insaan")
 game.active_map.place_item("Busy Gurpreet")
 
