a973f0b853340b37c1b0ab07dd362be58d6cab00
James471 <56861963+James471@users.noreply.github.com>
2022-03-30 20:02:03 +0530

    Bug fix

--- assets/items/code/fact8.py
+++ assets/items/code/fact8.py
@@ -1,5 +1,5 @@
 game.factsFound[7] = 1
 
 if sum(game.factsFound) == 15:
-    game.map("PhD Room").remove_item("Bored PhD")
-    game.map("PhD Room").place_item("Gurpreet Sita Rahim Mary Insaan")
+    game.map("PhD Room").remove_item("Bored PhD", game)
+    game.map("PhD Room").place_item("Gurpreet Sita Rahim Mary Insaan", game)
